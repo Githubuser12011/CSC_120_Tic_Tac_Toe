32bd89527e04b7989c540f399ddac5b7f09810d7
brojas1200 <bryanherbert928@gmail.com>
2021-12-01 13:50:40 -0500

    add player input

--- board.py
+++ board.py
@@ -8,6 +8,21 @@ board = [
 
 ]
 
+piecetype = "Pick your piece:"
+rowmess = "select your row:"
+colmess = "select your column:"
 
+player1piece = None
+player2piece = None
 
+player1piece = str(input("player1"+ piecetype))
+row = int(input("player1" + rowmess))
+col = int(input("player1" + colmess))
+board[row][col] = player1piece
+
+player2piece = str(input("player2"+ piecetype))
+row = int(input("player2" + rowmess))
+col = int(input("player2" + colmess))
+board[row][col] = player2piece
 print(board)
+
