8a89076ea501eedf701cd6d9f4efbfaae51a6345
Sparkypen7 <95368892+Githubuser12011@users.noreply.github.com>
2021-12-02 15:21:23 -0500

    Update board.py

--- board.py
+++ board.py
@@ -4,21 +4,64 @@ board = [
     ['-', '-', '-'],
 ]
 
-piecetype = "Pick your piece: "
-rowmess = "select your row: "
-colmess = "select your column: "
+piecetype = " Pick your piece: "
+rowmess = " select your row: "
+colmess = " select your column: "
 
 player1piece = None
 player2piece = None
 
-player1piece = str(input("player1"+ piecetype))
-row = int(input("player1" + rowmess))
-col = int(input("player1" + colmess))
-board[row][col] = player1piece
+inputStack = []
 
-player2piece = str(input("player2"+ piecetype))
-row = int(input("player2" + rowmess))
-col = int(input("player2" + colmess))
-board[row][col] = player2piece
+def addInputToStack(row, col):
+    inputStack.append([row, col])
+    for rowColPair in inputStack:
+        print(rowColPair)
+
+
+def handlePlayerInput(player):
+    invalidInput = False
+
+    row = int(input(player + rowmess))
+
+    if row < 0 or row > 2:
+        invalidInput = True
+
+    while invalidInput:
+        print("Invalid input.")
+        row = int(input(player + rowmess))
+        if not row < 0 or not row > 2:
+            invalidInput = False 
+    
+    col = int(input(player + colmess))
+
+    if col < 0 or col > 2:
+        invalidInput = True
+
+    while invalidInput:
+        print("Invalid input.")
+        col = int(input(player + colmess))
+        if not col < 0 or not col > 2:
+            invalidInput = False
+
+    addInputToStack(row, col)
+    return row, col
+
+
+def handlePlayer(player):
+    if player == "player1":
+        player1piece = str(input(player + piecetype))
+        row, col = handlePlayerInput(player)
+        
+        board[row][col] = player1piece
+    elif player == "player2":
+        player2piece = str(input(player + piecetype))
+        row, col = handlePlayerInput(player)
+        board[row][col] = player2piece        
+    else:
+        print("Invalid player")
+
+handlePlayer("player1")
+handlePlayer("player2")
 print(board)
 
