8cd6ee8560ad1aef42eb3da2d31182c6dc8fa45a
Sparkypen7 <95368892+Githubuser12011@users.noreply.github.com>
2021-12-02 16:25:59 -0500

    check for duplicate answers

--- board.py
+++ board.py
@@ -13,11 +13,19 @@ player2piece = None
 
 inputStack = []
 
-def addInputToStack(row, col):
+def checkDuplicates(row, col):
+    invalidInput = False
     inputStack.append([row, col])
-    for rowColPair in inputStack:
-        print(rowColPair)
+    if len(inputStack) == 2:
+        if (inputStack[0] == inputStack[1]):
+            invalidInput = True
+            inputStack.pop()
+        else:
+            invalidInput = False
 
+    if invalidInput:
+        print("Invalid input. Duplicate entry.")
+        handlePlayer("player2")
 
 def handlePlayerInput(player):
     invalidInput = False
@@ -44,20 +52,37 @@ def handlePlayerInput(player):
         if not col < 0 or not col > 2:
             invalidInput = False
 
-    addInputToStack(row, col)
+    checkDuplicates(row,col)
+    
     return row, col
 
 
 def handlePlayer(player):
+    takenSymbol = None
+
     if player == "player1":
         player1piece = str(input(player + piecetype))
-        row, col = handlePlayerInput(player)
-        
-        board[row][col] = player1piece
+        if player1piece == 'x' or player1piece == 'o':
+            takenSymbol = player1piece
+            row, col = handlePlayerInput(player)
+            board[row][col] = player1piece
+        else:
+            print("Symbol must be \'x\' or \'o\'")
+            handlePlayer("player1")
+
     elif player == "player2":
         player2piece = str(input(player + piecetype))
-        row, col = handlePlayerInput(player)
-        board[row][col] = player2piece        
+        if player2piece == 'x' or player2piece == 'o' and player2piece != takenSymbol:
+            row, col = handlePlayerInput(player)
+            board[row][col] = player2piece
+        elif player2piece == takenSymbol:
+            if takenSymbol == 'x':
+                print("Symbol taken. Must use \'o\'")
+            else:
+                print("Symbol taken. Must use \'x\'")
+        else:
+            print("Symbol must be \'x\' or \'o\'")
+            handlePlayer("player2")      
     else:
         print("Invalid player")
 
