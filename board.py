843c8de18e816f06a20c7a880b201dfbac7705a2
Sparkypen7 <95368892+Githubuser12011@users.noreply.github.com>
2021-12-05 17:35:39 -0500

    Update board logic

--- board.py
+++ board.py
@@ -15,13 +15,49 @@ colmess = " Select your column: "
 player1piece = None
 player2piece = None
 
-# inputStack is a stack to make sure there are no duplicates.
-inputStack = []
-
 selectedPieces = {
     "player1": None,
     "player2": None
 }
+
+# Board logic:
+def bindToBoard(functionToBind, playerToBind):
+    bindedReturn = None
+    for row in range(len(board)):
+        for row_element in range(len(board)):
+            bindedReturn = functionToBind(row, row_element, playerToBind)
+    return bindedReturn
+def printBoard(row, row_element):
+    print(row, row_element)
+def resetBoard(row, row_element):
+    board[row][row_element] = '-'
+def checkColumns(row, row_element, player):
+    hasWon = True
+    if board[row][row_element] != selectedPieces[player]:
+        hasWon = False
+    return hasWon
+def checkRows(row, row_element, player):
+    hasWon = True
+    if board[row][row_element] != selectedPieces[player]:
+        hasWon = False
+    return hasWon
+def checkDiagonal(row, row_element, player):
+    hasWon = True
+    if board[row][row] != selectedPieces[player]:
+        hasWon = False
+    return hasWon
+def checkInverseDiagonal(row, row_element, player):
+    hasWon = True
+    if board[row][len(board) - row] != selectedPieces[player]:
+        hasWon = False
+    return hasWon
+def checkFullBoard(row, row_element):
+    if board[row][row_element] == '-':
+        return False
+    return True
+
+# inputStack is a stack to make sure there are no duplicates.
+inputStack = []
 
 def checkDuplicates(player, row, col):
     invalidInput = False
@@ -39,46 +75,46 @@ def checkDuplicates(player, row, col):
 
 def handlePlayerInput(player):
     invalidInput = False
+    if bindToBoard(checkColumns, player) or bindToBoard(checkRows, player) or bindToBoard(checkDiagonal, player) or bindToBoard(checkInverseDiagonal, player) or bindToBoard(checkFullBoard, player):
+        print(player + " has won!")
+    else:
+        try:
+            row = int(input(player + rowmess))
+            assert row >= 0
+            assert row < 3
+        except ValueError:
+            print("Row must be an integer.")
+            handlePlayerInput(player)
+        except AssertionError:
+            invalidInput = True
 
-    try:
-        row = int(input(player + rowmess))
-        assert row >= 0
-        assert row < 3
-    except ValueError:
-        print("Row must be an integer.")
-        handlePlayerInput(player)
-    except AssertionError:
-        invalidInput = True
-    
-    while invalidInput:
-        print("Invalid input.")
-        row = int(input(player + rowmess))
-        if row >= 0 and row < 3:
-            invalidInput = False
-    
-    try:
-        col = int(input(player + colmess))
-        assert col >= 0
-        assert col < 3
-    except ValueError:
-        print("Column must be an integer.")
-        handlePlayerInput(player)
-    except AssertionError:
-        invalidInput = True
+        while invalidInput:
+            print("Invalid input.")
+            row = int(input(player + rowmess))
+            if row >= 0 and row < 3:
+                invalidInput = False
 
-    while invalidInput:
-        print("Invalid input.")
-        col = int(input(player + colmess))
-        if col >= 0 and col < 3:
-            invalidInput = False
+        try:
+            col = int(input(player + colmess))
+            assert col >= 0
+            assert col < 3
+        except ValueError:
+            print("Column must be an integer.")
+            handlePlayerInput(player)
+        except AssertionError:
+            invalidInput = True
 
-    checkDuplicates(player, row, col)
-    
+        while invalidInput:
+            print("Invalid input.")
+            col = int(input(player + colmess))
+            if col >= 0 and col < 3:
+                invalidInput = False
+        checkDuplicates(player, row, col)
     return row, col
 
 def handlePlayer(player):
     assert player == "player1" or player == "player2"
-    
+
     if player == "player1":
         try:
             if selectedPieces[player] == None:
@@ -91,7 +127,12 @@ def handlePlayer(player):
 
         try: 
             row, col = handlePlayerInput(player)
-            board[row][col] = player1piece
+            try: 
+                assert board[row][col] == '-'
+                board[row][col] = player1piece
+            except AssertionError:
+                print("Spot occupied already.")
+                handlePlayerInput(player)
         except:
             pass
     elif player == "player2":
@@ -108,31 +149,27 @@ def handlePlayer(player):
 
         try: 
             row, col = handlePlayerInput(player)
-            board[row][col] = player2piece
+            try: 
+                assert board[row][col] == '-'
+                board[row][col] = player2piece
+            except AssertionError:
+                print("Spot occupied already.")
+                handlePlayerInput(player)
         except:
             pass
-
-def bindToBoard(functionToBind):
-    for row in range(len(board)):
-        for row_element in range(len(board)):
-            functionToBind(row, row_element)
-    
-
-def resetBoard(row, row_element):
-    board[row][row_element] = '-'
-
-def validateWinner(row, row_element):
-    return "hi"
-    # print(row, row_element)
-
 
 # isGameRunning = False
 
 bindToBoard(resetBoard)
 
 while isGameRunning:
-    handlePlayer("player1")
-    handlePlayer("player2")
-    print(board)
+    if not selectedPieces["player1"] == None and not selectedPieces["player2"] == None:
+        handlePlayerInput("player1")
+        handlePlayerInput("player2")
+    else:
+        handlePlayer("player1")
+        handlePlayer("player2")
+
+    bindToBoard(printBoard)
  
 
